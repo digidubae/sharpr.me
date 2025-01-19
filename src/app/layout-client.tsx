dd342d26877493d645b7385545c5ec4567aece84
digidub <digidub.ae@gmail.com>
2025-01-19 21:41:36 +0400

    double render syncstatus issue fix

--- src/app/layout-client.tsx
+++ src/app/layout-client.tsx
@@ -15,7 +15,7 @@ function HeaderButtons() {
     <>
       {isInSpace && (
         <div className="fixed top-4 left-4 z-50">
-          <SyncStatus />
+          {/* <SyncStatus /> */}
         </div>
       )}
       <div className="fixed top-4 right-4 z-50">
