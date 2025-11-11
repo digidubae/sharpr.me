22be2a06933a1fa081d91f3ff9084b74f6cfa5f0
digidub <digidub.ae@gmail.com>
2025-11-11 10:32:41 +0400

    fixed manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,8 +10,8 @@ export default function manifest(): MetadataRoute.Manifest {
 		theme_color: '#000000',
 		icons: [
 			// Recommended PNGs for installability:
-			{ src: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
-			{ src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+			{ src: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+			{ src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
 			// Keep SVG as a scalable fallback
 			{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any' }
 		]
